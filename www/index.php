34e0dca14d3381f94174cb35e7bab15b3d8a92c1
Adric Schreuders <adric@astrepo.com.au>
2014-06-19 02:04:02 +0800

    Update index.php to output errors in the right format

--- www/index.php
+++ www/index.php
@@ -1,51 +1,114 @@
-<?
+<?php
+
 if (!isset($_SERVER) || !isset($_SERVER['DOCUMENT_ROOT'])) {
     throw new Exception('DOCUMENT_ROOT must be defined in the server environment variables');
 }
 define('WEB_ROOT', $_SERVER['DOCUMENT_ROOT'] . '/');
 define('BASE_DIR', WEB_ROOT . '../');
 define('CONFIG_DIR', BASE_DIR . 'config/');
-define('UTIL_DIR', BASE_DIR . 'core/utils/');
 
 require_once(BASE_DIR . 'core/structure/exceptions.php');
-require_once(BASE_DIR . 'core/Environment.php');
-require_once(BASE_DIR . 'core/HTTPEnvironment.php');
-require_once(UTIL_DIR . 'Config.php');
-require_once(UTIL_DIR . 'cache/Cache.php');
-require_once(UTIL_DIR . 'cache/APC.php');
-require_once(UTIL_DIR . 'Autoloader.php');
+require_once(BASE_DIR . 'core/environment/Environment.php');
+require_once(BASE_DIR . 'core/environment/HTTPEnvironment.php');
+require_once(BASE_DIR . 'core/utils/Config.php');
+require_once(BASE_DIR . 'core/utils/dataAccess/cache/Cache.php');
+require_once(BASE_DIR . 'core/utils/dataAccess/cache/APC.php');
+require_once(BASE_DIR . 'core/vendor/autoload.php');
+require_once(BASE_DIR . 'core/utils/Autoloader.php');
 
 $env = new HTTPEnvironment();
+$format = $env->getFormat();
 
-try {
-    $route = new Route($env->uri(), $env->getConfig('routing'));
-} catch (RouteException $e) {
-    $view = new NotFoundView($env->getConfig('template'), $env->uri());
-    echo $view->generateView();
-    exit();
-}
+$route = RoutingFactory::getRoute();
 
+// Check for redirect
 if ($redirect = $route->getRedirect()) {
     header('Location: ' . $redirect, true, 301);
     exit();
-} else {
-    $class = $route->getClassName();
-    $function = $route->getFunctionName();
-    $param = $route->getParameterValue();
 
-    try {
-        $controller = new $class($env);
-        if ($param) {
-            $output = $controller->$function($param);
+// File Not Found
+} else if (!($class = $route->getClassName())) {
+    notFound($format, $route->getUri());
+    exit();
+}
+
+$function = $route->getFunctionName();
+$params = $route->getParameterValues();
+
+// Execute controller
+try {
+    $auth = new HTTPAuth($env->input());
+    $controller = new $class($env->getConfig(), $env->input(), $auth);
+    if ($params) {
+        $output = call_user_method_array($function, $controller, $params);
+    } else {
+        $output = $controller->$function();
+    }
+    echo $output;
+
+} catch (NotFoundException $e) {
+    notFound($format, $route->getUri());
+
+// User safe error message (usually invalid input, etc)
+} catch (UserSafeException $e) {
+    userError($format, $e);
+
+// Unexpected Exception
+} catch (Exception $e) {
+    serverError($format, $e, $env->isProduction());
+}
+
+
+function notFound($format, $uri) {
+    http_response_code(404);
+    if ($format == 'plain') {
+        echo "Resource Not Found\nThere is no resource located at $uri\n";
+    } else {
+        if ($format == 'html') {
+            $view = ViewFactory::createView('NotFound');
+            $view->setResource($uri);
         } else {
-            $output = $controller->$function();
+            $view = ViewFactory::createDataView();
+            $view->setError("There is no resource located at $uri");
         }
-        echo $output;
-    } catch (UserSafeException $e) {
-        $view = new ServerErrorView($env->getConfig('template'), $e->getMessage());
         echo $view->generateView();
-    } catch (Exception $e) {
-        $view = new ServerErrorView($env->getConfig('template'));
+    }
+}
+
+function userError($format, $e) {
+    http_response_code(400);
+    if ($format == 'plain') {
+        echo "Server Error\n" . $e->getMessage() . "\n";
+    } else {
+        if ($format == 'html') {
+            $view = ViewFactory::createView('BadRequest');
+        } else {
+            $view = ViewFactory::createDataView(null, $format);
+        }
+        $view->setError($e->getMessage());
+        echo $view->generateView();
+    }
+}
+
+function serverError($format, $e, $production = false) {
+    http_response_code(500);
+    if ($format == 'plain') {
+        echo "Server Error\n";
+        if (!$production) {
+            echo $e->getMessage() . "\n";
+        }
+    } else {
+        if ($format == 'html') {
+            $view = ViewFactory::createView('ServerError');
+        } else {
+            $view = ViewFactory::createDataView();
+            if ($production) {
+                $view->setError('Server Error');
+            }
+        }
+        if (!$production) {
+            $view->setError($e->getMessage());
+        }
         echo $view->generateView();
     }
 }
