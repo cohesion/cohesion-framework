af670de6fd566fd64ab06cffebcc6ea809da1e0f
Adric Schreuders <adric@astrepo.com.au>
2014-06-19 00:06:36 +0800

    Create database interface

---
+++ core/utils/dataAccess/Database.php
@@ -0,0 +1,15 @@
+<?php
+
+interface Database {
+    public function query($query, $binds = null);
+
+    public function queryMaster($query, $binds = null);
+
+    public function querySlave($query, $binds = null);
+
+    public function startTransaction();
+
+    public function rollback();
+
+    public function commit();
+}
---
+++ core/utils/dataAccess/DatabaseResult.php
@@ -0,0 +1,23 @@
+<?php
+
+interface DatabaseResult {
+
+    public function row($index);
+
+    public function nextRow();
+
+    public function result();
+
+    public function getQuery();
+
+    public function insertId();
+
+    public function numRows();
+
+    public function numAffectedRows();
+
+    public function numCols();
+
+    public function isEmpty();
+}
+
