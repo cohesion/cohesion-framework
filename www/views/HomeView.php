c1f1d693af0a65f9367a3d033268d5730a9551f7
Adric Schreuders <adric@schreuders.org>
2014-08-06 09:10:28 +0800

    Update default home page

--- www/views/HomeView.php
+++ www/views/HomeView.php
@@ -4,6 +4,7 @@ class HomeView extends MyView {
     public function __construct($template, $engine, $vars) {
         parent::__construct($template, $engine, $vars);
         $vars['page'] = 'home';
+        $vars['title'] = $vars['site_name'] . ' Home';
         $this->addVars($vars);
     }
 }
